0251babfacae39bff39d2c742222665aa18e1e01
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2023-11-06 11:15:29 -0300

    V3

--- backend/rotas/salvar_imagem.py
+++ backend/rotas/salvar_imagem.py
@@ -1,12 +1,13 @@
 from backend.geral.config import *
+from backend.imagens.caminho import caminho_imagens
 
 @app.route("/save_image", methods=['POST'])
 def salvar_imagem():
     try:
         #print("comecando")
-        file_val = request.files['foto']
+        file_val = request.files['files']
         #print("vou salvar em: "+file_val.filename)
-        arquivoimg = os.path.join(path, 'backend/imagens/'+file_val.filename)
+        arquivoimg = os.path.join(caminho_imagens, 'passaro.png')
         file_val.save(arquivoimg)
         r = jsonify({"resultado":"ok", "detalhes": file_val.filename})
     except Exception as e:
